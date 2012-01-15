73a158f889d17bf4f6381e71d732ab97c87bf247
Alex Kudryashov <alex.kudryashov@gmail.com>
2012-01-16 00:08:21 +0100

    Moved vub conf inside and deleted sh script

--- vu.sh
+++
@@ -1,8 +0,0 @@
-cd /Users/alexander/.vim/vub
-git pull
-cd /Users/alexander/.vim
-./vub/vim-update-bundles \
-    --vimdir-path=/Users/alexander/.vim \
-    --vimrc-path=/Users/alexander/.vim/.vimrc \
-    --verbose
-
